6710c444ec9dddad7c1e7d4e4f08a0800fe59e06
Aanish <aanishamir@gmail.com>
2021-08-24 00:49:03 +0500

    utils pushed

--- utils.py
+++ utils.py
@@ -4,10 +4,20 @@ def parse_message(msg):
     extracts buy/sell, symbol and percentage
     :return:
     """
-    msg_dict = {}
-    msg_list = msg.split("-")
-    msg_dict["buy_or_sell"] = msg_list[0]
-    msg_dict["symbol"] = msg_list[1]
-    msg_dict["percentage"] = msg_list[2]
+    # msg_dict = {}
+    # msg_list = msg.split("-")
+    # msg_dict["buy_or_sell"] = msg_list[0]
+    # msg_dict["symbol"] = msg_list[1]
+    # msg_dict["percentage"] = msg_list[2]
 
-    return msg_dict
+    order_details = msg.split(' ')
+
+    msg_dic = {
+        'side': order_details[0],
+        'symbol': order_details[1],
+        'token': order_details[2],
+        'percentage': order_details[3],
+    }
+
+    return msg_dic
+    # return msg_dict
