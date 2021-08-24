2f5c65a9211e2fddce1532e0fa8e75c2613f213c
Muhammad Hamza Ashraf <mhamza.1410@gmail.com>
2021-08-25 00:47:33 +0500

    1-Async-Binance-Trading: Asynchronous implementation.

--- binance_integration.py
+++ binance_integration.py
@@ -1,8 +1,10 @@
 from binance.enums import *
 from binance.client import Client
+from binance import AsyncClient
 from gevent import os
 from dotenv import load_dotenv
 from utils import *
+import asyncio
 
 load_dotenv()
 
@@ -11,17 +13,12 @@ class Connect:
 
     client = None
 
-    def __init__(self, api_key=None, api_secret=None) -> None:
-        """
-        we can pass in the api_keu and the api_secret key when we move onto the telgeram parsing part
-        :param api_key:
-        :param api_secret:
-        """
+    async def create_client(self):
         api_key = os.environ.get('api_key')
         api_secret = os.environ.get('api_secret')
-        self.client = Client(api_key, api_secret, testnet=True)
+        self.client = AsyncClient(api_key, api_secret, testnet=True)
 
-    def place_order(self, order_msg="", _type='limit') -> None:
+    async def place_order(self, order_msg="", _type='limit') -> None:
         """
         this places an order, need to change the hardcoded values
         :return:
@@ -35,44 +32,52 @@ class Connect:
 
             checked_quantity = 0
             if _type == 'limit':
-                order = self.client.order_limit_buy(
+                order = await self.client.order_limit_buy(
                         symbol="{0}{1}".format(order_msg["symbol"], order_msg["token"]),
                         quantity=order_msg['percentage'],
                         price=1)
             if _type == 'market':
-                order = self.client.order_market_buy(
+                order = await self.client.order_market_buy(
                         symbol="{0}{1}".format(order_msg["symbol"], order_msg["token"]),
                         quantity=order_msg['percentage'])
 
             print(order)
 
-    def get_history(self, symbol='XRPBNB') -> None:
+    async def get_history(self, symbol='XRPBNB') -> None:
         """
         :return:
         """
-        orders = self.client.get_my_trades(symbol=symbol)
-
+        orders = await self.client.get_my_trades(symbol=symbol)
         print(orders)
 
-    def get_balance(self, asset='USDT'):
+    async def get_balance(self, asset='USDT'):
         """
         :param asset: the tokens we want to check the balance for in our account
         :return:
         """
-        balance = self.client.get_asset_balance(asset=asset)
+        balance = await self.client.get_asset_balance(asset=asset)
         print(balance)
         return balance
 
+    async def close_connection(self):
+        await self.client.close_connection()
 
-if __name__ == '__main__':
+
+async def main():
     # need to replace this msg with the ones from telegram later on
     # temp_msg1 = "buy-XRPBNB-10"
     # temp_msg2 = "sell-ADABTC-20"
     temp_msg2 = "buy XRP USDT 20"
-    order_msg = parse_message(temp_msg2)
+    order_msg = await parse_message(temp_msg2)
     client = Connect()
-    # place_order(client)
-    client.get_balance('USDT')
-    client.place_order(order_msg)
-    client.get_history()
-    client.get_balance('USDT')
+    await client.create_client()
+    await client.get_balance('USDT')
+    await client.place_order(order_msg)
+    await client.get_history()
+    await client.get_balance('USDT')
+    await client.close_connection()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
